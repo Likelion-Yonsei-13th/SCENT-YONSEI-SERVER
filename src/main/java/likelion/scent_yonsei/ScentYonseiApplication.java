4017bea7205c48c9f756d552ba5fd3926d2aa865
Hoyoung027 <hybyun0207@gmail.com>
2025-05-05 01:23:59 +0900

    [INIT] initial setting

---
+++ src/main/java/likelion/scent_yonsei/ScentYonseiApplication.java
@@ -0,0 +1,13 @@
+package likelion.scent_yonsei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScentYonseiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScentYonseiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/likelion/scent_yonsei/ScentYonseiApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion.scent_yonsei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScentYonseiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
