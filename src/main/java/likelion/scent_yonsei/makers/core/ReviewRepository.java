d6538325b790e434984380d789cd46109a6b652f
soojung <crystal007002@naver.com>
2025-05-15 18:32:17 +0900

    [FEAT] review 받아오기

---
+++ src/main/java/likelion/scent_yonsei/makers/core/ReviewRepository.java
@@ -0,0 +1,7 @@
+package likelion.scent_yonsei.makers.core;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+}
