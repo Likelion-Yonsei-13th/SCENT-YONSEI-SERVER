1d061c8fe320413ac2c3f4d2d587355c07b6a5ff
Hoyoung027 <hybyun0207@gmail.com>
2025-05-17 02:10:19 +0900

    [FEAT] 공연 관련 Repository 설정

---
+++ src/main/java/likelion/scent_yonsei/domain/show/core/Show.java
@@ -0,0 +1,61 @@
+package likelion.scent_yonsei.domain.show.core;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "show")
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Show {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 30, nullable = false)
+    private String name;
+
+    @Column(length = 50)
+    private String title;
+
+    private Integer day;
+
+    @Column(name = "start_at")
+    private LocalDateTime startAt;
+
+    @Column(name = "finish_at")
+    private LocalDateTime finishAt;
+
+    @Column(length = 10)
+    private String section;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(columnDefinition = "TEXT")
+    private String instagram;
+
+    @Column(length = 100)
+    private String photo;
+
+    private Long liked;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "is_student")
+    private Boolean student;
+
+}
---
+++ src/main/java/likelion/scent_yonsei/domain/show/core/ShowRepository.java
@@ -0,0 +1,8 @@
+package likelion.scent_yonsei.domain.show.core;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShowRepository extends JpaRepository<Show, Long> {
+}
