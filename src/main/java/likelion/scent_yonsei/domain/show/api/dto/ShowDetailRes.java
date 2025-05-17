9c216c65d72d30a8be894d07e303bb576c5a21bb
Hoyoung027 <hybyun0207@gmail.com>
2025-05-18 03:58:59 +0900

    [FIX] 공연 관련 dto 수정

--- src/main/java/likelion/scent_yonsei/domain/show/api/dto/ShowDetailRes.java
+++ src/main/java/likelion/scent_yonsei/domain/show/api/dto/ShowDetailRes.java
@@ -2,15 +2,18 @@ package likelion.scent_yonsei.domain.show.api.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.List;
+
 public record ShowDetailRes(
         @JsonProperty("showId") Long showId,
         String title,
         String name,
         String instagram,
         String section,
+        String day,
         String start_at,
         String finish_at,
         String description,
-        String photo
+        List<String> photo
 ) {
 }
