bdb6d77bc0b08aca4d9098803b3072ae5b73e31c
Hoyoung027 <hybyun0207@gmail.com>
2025-05-22 01:43:52 +0900

    [FIX] fix length of menu name

--- src/main/java/likelion/scent_yonsei/domain/booth/core/BoothMenu.java
+++ src/main/java/likelion/scent_yonsei/domain/booth/core/BoothMenu.java
@@ -12,7 +12,7 @@ public class BoothMenu {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 100)
+    @Column(nullable = false, length = 255)
     private String name;
 
     @Column(nullable = false)
